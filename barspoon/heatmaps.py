4b09200dbe0c499e3b13720f47205bd3e0f360c7
Marko van Treeck <markovantreeck@gmail.com>
2023-07-06 16:43:24 +0200

    Make heatmaps using grad-cam alone

--- barspoon/heatmaps.py
+++ barspoon/heatmaps.py
@@ -34,7 +34,7 @@ def main():
 
     target_labels = model.hparams["target_labels"]
 
-    # load dataset, grouped by filename
+    # Load dataset, grouped by filename
     dataset_df = make_dataset_df(
         clini_tables=args.clini_tables,
         slide_tables=args.slide_tables,
@@ -58,57 +58,73 @@ def main():
             xs = np.sort(np.unique(coords[:, 0]))
             stride = np.min(xs[1:] - xs[:-1])
 
-        att_maps = compute_attention_maps(model, feats, coords, stride, args.batch_size)
-        score_maps = compute_score_maps(model, feats, coords, stride)
-
-        mask = (att_maps > 0).any(axis=0)
+        gradcams = compute_attention_maps(model, feats, coords, stride, args.batch_size)
+        mask = (gradcams > 0).any(axis=0)
 
         categories = model.hparams["categories"]
         target_edges = np.cumsum([0, *(len(c) for c in categories)])
 
         # Save all the heatmaps
         (outdir := args.output_dir / h5_path.stem).mkdir(exist_ok=True, parents=True)
-        for target_label, cs, left in zip(
+        for target_label, cs, left, right in zip(
             target_labels,
             categories,
             target_edges[:-1],
+            target_edges[1:],
             strict=True,
         ):
-            for i, category in enumerate(cs):
-                # Save metadata in slide
-                att_im = plt.get_cmap("magma")(att_maps[left + i] / att_maps.max())
-                att_im[:, :, -1] = mask
-                Image.fromarray(
-                    np.uint8(255 * att_im),
-                    "RGBA",
-                ).save(
-                    outdir / f"attention_{target_label}_{category}.png",
-                    pnginfo=make_metadata(
-                        # metadata format semver
-                        # update minor version when adding fields,
-                        # major when removing fields / changing semantics
-                        version="barspoon-attention 1.0",
-                        filename=h5_path.stem,
-                        stride=str(stride),
-                        target_label=target_label,
-                        category=category,
-                        attention_scale=f"{att_maps.max():e}",
-                    ),
+            gradcam_im = plt.get_cmap("magma")(
+                abs(gradcams[left]) / abs(gradcams).max()
+            )
+            gradcam_im[:, :, -1] = mask
+            Image.fromarray(
+                np.uint8(255 * gradcam_im),
+                "RGBA",
+            ).resize(
+                np.array(mask.shape)[::-1] * 8, resample=Image.Resampling.NEAREST
+            ).save(
+                outdir / f"gradcam_absolute_{target_label}.png",
+                pnginfo=make_metadata(
+                    # Metadata format semver
+                    # Update minor version when adding fields,
+                    # Major when removing fields / changing semantics
+                    version="barspoon-absolute-gradcam 1.0",
+                    filename=h5_path.stem,
+                    stride=str(stride / 8),
+                    target_label=target_label,
+                    scale=f"{abs(gradcams).max():e}",
+                ),
+            )
+            for category, cat_gradcam in zip(cs, gradcams[left:right], strict=True):
+                # Both the class-indicating map and the alpha map contain information
+                # on the "importance" of a region,
+                # where the class-indicating map becomes more faint for low attention regions
+                # and the alpha becomes more transparent.
+                # Since those two factors of faintness would otherwise multiplicatively compound
+                # we take the square root of both, counteracting the effect.
+                gradcam_im = plt.get_cmap("coolwarm")(
+                    np.sign(cat_gradcam)
+                    * np.sqrt(abs(cat_gradcam) / abs(cat_gradcam).max())
+                    / 2
+                    + 0.5
                 )
-
-                # use scores as color, attention as alpha
-                im = plt.get_cmap("coolwarm")(score_maps[left + i])
-                im[:, :, -1] = att_maps[left + i] / att_maps[left + i].max()
-
-                Image.fromarray(np.uint8(255 * im), "RGBA").save(
-                    outdir / f"map_{target_label}_{category}.png",
+                gradcam_im[:, :, -1] = np.sqrt(
+                    abs(cat_gradcam) / abs(cat_gradcam).max()
+                )
+                Image.fromarray(
+                    np.uint8(255 * gradcam_im),
+                    "RGBA",
+                ).resize(
+                    np.array(mask.shape)[::-1] * 8, resample=Image.Resampling.NEAREST
+                ).save(
+                    outdir / f"gradcam_{target_label}_{category}.png",
                     pnginfo=make_metadata(
-                        version="barspoon-map 1.0",  # see above
+                        version="barspoon-cat-gradcam 1.0",
                         filename=h5_path.stem,
-                        stride=str(stride),
+                        stride=str(stride / 8),
                         target_label=target_label,
                         category=category,
-                        attention_scale=f"{att_maps[left+i].max():e}",
+                        scale=f"{abs(cat_gradcam).max():e}",
                     ),
                 )
 
@@ -127,7 +143,7 @@ def compute_attention_maps(
         torch.tensor(feats)
         .unsqueeze(0)
         .to(torch.bfloat16)
-        .repeat(batch_size, 1, 1)
+        .repeat(min(batch_size, n_outs), 1, 1)
         .cuda()
     )
     model = model.eval()
@@ -135,31 +151,16 @@ def compute_attention_maps(
         feats_t = feats_t.detach()  # Zero grads of input features
         feats_t.requires_grad = True
         model.zero_grad()
-        scores = model(feats_t).sigmoid()
+        scores = model.predict_step(feats_t, batch_idx=0)
         scores[:, idx:].diag().sum().backward()
 
-        gradcam = (feats_t.grad * feats_t).abs().sum(-1)
+        gradcam = (feats_t.grad * feats_t).sum(-1)
         atts.append(gradcam.detach().cpu())
 
-    # If n_outs isn't divisible by batch_size,
-    # we'll have some superfluous output maps
+    # If n_outs isn't divisible by batch_size, we'll have some superfluous output maps
     # Drop them
     atts = torch.cat(atts)[:n_outs]
     return vals_to_im(atts.permute(1, 0), coords, stride)
-
-
-def compute_score_maps(model, feats, coords, stride):
-    """Computes a stack of attention maps
-
-    Returns:
-        An array of shape [n_dim, x, y]
-    """
-    model = model.eval()
-    with torch.no_grad():
-        feats_t = torch.tensor(feats).to(torch.bfloat16).unsqueeze(1).cuda()
-        scores = model(feats_t).sigmoid().detach().cpu()
-    score_maps = vals_to_im(scores, coords, stride)
-    return score_maps
 
 
 def vals_to_im(scores, coords, stride) -> npt.NDArray[np.float_]:
